bc483ecb5da2d72d5a8bbbc0ea199a54114e966f
Smarter  Harder <33955773+NWarila@users.noreply.github.com>
2023-01-15 11:52:20 -0500

    Create Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,215 @@
+If ($False) {
+    $Titans = @{
+    4000 = @{
+        ID = 4000
+        Name = 'Sigurd'
+        Role = 'Tank'
+        Element = 'Water'
+        API = '{"id":4000,"x p":686760,"level":120,"star":6,"skills":{"4001":120},"power":185964,"skins":{"10001":60,"10013":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":11752763.21,"physicalAttack":778462.47,"elementArmor":139047,"elementAttack":422565,"elementSpiritPower":525000,"element":"water","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4001 = @{
+        ID = 4001
+        Name = 'Nova'
+        Role = 'Other'
+        Element = 'Water'
+        API = '{"id":4001,"xp":686760,"level":120,"star":6,"skills":{"4003":120},"power":170314,"skins":{"10002":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":6858164.15,"physicalAttack":1145574.02,"elementAttack":422565,"elementArmor":89982,"elementSpiritPower":525000,"element":"water","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4002 = @{
+        ID = 4002
+        Name = 'Mairi'
+        Role = 'Other'
+        Element = 'Water'
+        API = '{"id":4002,"xp":686760,"level":120,"star":6,"skills":{"4005":120},"power":170313,"skins":{"10003":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":9816958.55,"physicalAttack":923647.74,"elementAttack":422565,"elementArmor":89982,"elementSpiritPower":525000,"element":"water","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4003 = @{
+        ID = 4003
+        Name = 'Hyperion'
+        Role = 'Super'
+        Element = 'Water'
+        API = '{"id":4003,"xp":686760,"level":120,"star":6,"skills":{"4007":120,"4008":120},"power":170314,"skins":{"10004":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":10466148.379999999,"physicalAttack":874974.48,"elementAttack":422565,"elementArmor":89982,"elementSpiritPower":525000,"element":"water","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4010 = @{
+        ID = 4010
+        Name = 'Moloch'
+        Role = 'Tank'
+        Element = 'Fire'
+        API = '{"id":4010,"xp":686760,"level":120,"star":6,"skills":{"4010":120},"power":186000,"skins":{"10005":60,"10014":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":10687696.620000001,"physicalAttack":858868.05,"elementArmor":139047,"elementAttack":422565,"elementSpiritPower":1462500,"element":"fire","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4011 = @{
+        ID = 4011
+        Name = 'Vulcan'
+        Role = 'Other'
+        Element = 'Fire'
+        API = '{"id":4011,"xp":686760,"level":120,"star":6,"skills":{"4012":120},"power":170296,"skins":{"10006":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":5835277.66,"physicalAttack":1222027,"elementAttack":422565,"elementArmor":89982,"elementSpiritPower":1462500,"element":"fire","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4012 = @{
+        ID = 4012
+        Name = 'Ignis'
+        Role = 'Other'
+        Element = 'Fire'
+        API = '{"id":4012,"xp":686760,"level":120,"star":6,"skills":{"4014":120},"power":170313,"skins":{"10007":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":8762437.24,"physicalAttack":1002736.79,"elementAttack":422565,"elementArmor":89982,"elementSpiritPower":1462500,"element":"fire","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4013 = @{
+        ID = 4013
+        Name = 'Araji'
+        Role = 'Super'
+        Element = 'Fire'
+        API = '{"id":4013,"xp":686760,"level":120,"star":6,"skills":{"4016":120,"4017":120},"power":170274,"skins":{"10008":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":6586625.11,"physicalAttack":1165347.03,"elementAttack":422565,"elementArmor":89982,"elementSpiritPower":1462500,"element":"fire","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4020 = @{
+        ID = 4020
+        Name = 'Angus'
+        Role = 'Tank'
+        Element = 'Earth'
+        API = '{"id":4020,"xp":686760,"level":120,"star":6,"skills":{"4019":120},"power":185996,"skins":{"10009":60,"10015":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":16772882.059999999,"physicalAttack":402414.82,"elementArmor":139047,"elementAttack":422565,"elementSpiritPower":6750000,"element":"earth","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4021 = @{
+        ID = 4021
+        Name = 'Sylva'
+        Role = 'Other'
+        Element = 'Earth'
+        API = '{"id":4021,"xp":686760,"level":120,"star":6,"skills":{"4021":120},"power":170278,"skins":{"10010":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":11444600.73,"physicalAttack":801066.1799999999,"elementAttack":422565,"elementArmor":89982,"elementSpiritPower":6750000,"element":"earth","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4022 = @{
+        ID = 4022
+        Name = 'Avalon'
+        Role = 'Other'
+        Element = 'Earth'
+        API = '{"id":4022,"xp":686760,"level":120,"star":6,"skills":{"4023":120},"power":170313,"skins":{"10011":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":14392848.86,"physicalAttack":580457.44,"elementAttack":422565,"elementArmor":89982,"elementSpiritPower":6750000,"element":"earth","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4023 = @{
+        ID = 4023
+        Name = 'Eden'
+        Role = 'Super'
+        Element = 'Earth'
+        API = '{"id":4023,"xp":686760,"level":120,"star":6,"skills":{"4025":120,"4026":120},"power":170336,"skins":{"10012":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":13620313.35,"physicalAttack":638741.97,"elementAttack":422565,"elementArmor":89982,"elementSpiritPower":6750000,"element":"earth","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4031 = @{
+        ID = 4031
+        Name = 'Keros'
+        Role = 'Other'
+        Element = 'Dark'
+        API = '{"id":4031,"xp":686760,"level":120,"star":6,"skills":{"4030":120},"power":195957,"skins":{"10017":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":10289902.83,"physicalAttack":927608.46,"elementAttack":422565,"elementArmor":161967,"elementSpiritPower":2340000,"element":"dark","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4032 = @{
+        ID = 4032
+        Name = 'Mort'
+        Role = 'Other'
+        Element = 'Dark'
+        API = '{"id":4032,"xp":686760,"level":120,"star":6,"skills":{"4032":120},"power":195967,"skins":{"10018":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":13268469.24,"physicalAttack":704363.65,"elementAttack":422565,"elementArmor":161967,"elementSpiritPower":2340000,"element":"dark","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4041 = @{
+        ID = 4041
+        Name = 'Amon'
+        Role = 'Other'
+        Element = 'Light'
+        API = '{"id":4041,"xp":686760,"level":120,"star":6,"skills":{"4039":120},"power":195950,"skins":{"10021":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":7083572.48,"physicalAttack":1167983.1,"elementAttack":422565,"elementArmor":161967,"elementSpiritPower":5400000,"element":"light","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+    4042 = @{
+        ID = 4042
+        Name = 'Iyari'
+        Role = 'Other'
+        Element = 'Light'
+        API = '{"id":4042,"xp":686760,"level":120,"star":6,"skills":{"4041":120},"power":195965,"skins":{"10022":60},"currentSkin":0,"artifacts":[{"level":120,"star":6},{"level":120,"star":6},{"level":120,"star":6}],"scale":0.8,"type":"titan","perks":null,"anticrit":1,"antidodge":1,"hp":10080591.379999999,"physicalAttack":943420.75,"elementAttack":422565,"elementArmor":161967,"elementSpiritPower":5400000,"element":"light","elementSpiritLevel":120,"elementSpiritStar":6,"skin":0}'
+    }
+}
+
+    #Create Results Object
+    $Teams   = [System.Collections.ArrayList]::new()
+    $Results = [System.Collections.ArrayList]::new()
+
+    #Generate various lists of titans.
+    $TankRegex = ('(({0}),?.*){{2}}' -f ($Titans.Values.Where({$_.Role -eq 'Tank'}).ID -join '|'))
+    $NoSuperRegex = ('^(?:(?!({0})).)*$' -f ($Titans.Values.Where({$_.Role -eq 'Super'}).ID -join '|'))
+    $SuperTitans = $Titans.Values.Where({$_.Role -eq 'Super'}).id
+
+    #Create the titan team list.
+    $TitansLeft0 = $Titans.Keys
+    ForEach ($SlotA in $TitansLeft0) {
+        $TitansLeft1 = $TitansLeft0.Where({$_ -ne $SlotA})
+        ForEach ($SlotB in $TitansLeft1) {
+            $TitansLeft2 = $TitansLeft1.Where({$_ -ne $SlotB})
+            ForEach ($SlotC in $TitansLeft2) {
+                $TitansLeft3 = $TitansLeft2.Where({$_ -ne $SlotC})
+                ForEach ($SlotD in $TitansLeft3) {
+                    $TitansLeft4 = $TitansLeft3.Where({$_ -ne $SlotD})
+                    ForEach ($SlotE in $TitansLeft4) {
+                        $TargetTitans = @{$SlotA = $Titans[$SlotA];$SlotB = $Titans[$SlotB];
+                            $SlotC = $Titans[$SlotC];$SlotD = $Titans[$SlotD];$SlotE = $Titans[$SlotE]
+                        }
+                        $Score = 0
+                        $Skip = $false
+                        $Totems = @{Water=$False;Fire=$False;Earth=$False;Light=$False;Dark=$False}
+
+
+                        #Ensure at least 3 of a single element are in
+                        Switch -regex (($TargetTitans.Values.ID -join ',')) {
+                            '(400[0-3],?.*){3}' { $Totems.Water = $True; $Score += 10 }
+                            '(400[0-3],?.*){4}' { $Score += 5}
+                            '(401[0-3],?.*){3}' { $Totems.Fire  = $True; $Score += 10 }
+                            '(401[0-3],?.*){4}' { $Score += 5}
+                            '(402[0-3],?.*){3}' { $Totems.Earth = $True; $Score += 10 }
+                            '(402[0-3],?.*){4}' { $Score += 5}
+                            '(403[0-3],?.*){2}' { $Totems.Dark  = $True; $Score += 4  }
+                            '(404[0-3],?.*){2}' { $Totems.Light = $True; $Score += 4  }
+                            $TankRegex          { $Skip = $True}
+                            $NoSuperRegex       { $Skip = $True}
+                            Default {$Skip = $True}
+                        }
+
+                        #Calculate value of super titans.
+                        $Score += ($TargetTitans.Values.Where({$_.Role -eq 'Super'}).count * 2)
+
+                        If (!$Totems.Earth -and !$Totems.Water -and !$Totems.Fire) {
+                            $Skip = $True
+                        }
+
+                        If ($Skip -eq $False) {
+                            $Null = $Teams.Add([PSCustomObject]@{
+                                Score  = $Score
+                                Titan1 = $SlotA
+                                Titan2 = $SlotB
+                                Titan3 = $SlotC
+                                Titan4 = $SlotD
+                                Titan5 = $SlotE
+                            })
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+ForEach ($Defender in ($Teams |Sort-Object -Descending -Property:'Score')) {
+    $CounterFound = $false
+    $Counter = 0
+    While (!$CounterFound -and $Counter -lt $Teams.count) {
+    #Write-Host "Testing: $($Titans[$Teams[$Counter].Titan1].name)-$($Titans[$Teams[$Counter].Titan2].name)-$($Titans[$Teams[$Counter].Titan3].name)-$($Titans[$Teams[$Counter].Titan4].name)-$($Titans[$Teams[$Counter].Titan5].name)"
+    $Battle = Invoke-WebRequest -UseBasicParsing -Uri "http://10.69.2.7:8080/battle" `
+        -Method "POST" -WebSession $session -Headers @{"Referer"="";"DNT"="1"} `
+        -ContentType "application/json" -Body:(('{"battleData": { "userId": 12491146,"typeId": 12491146,"attackers": {') +
+        ('"{0}": {1},"{2}": {3},"{4}": {5},"{6}": {7},"{8}": {9}' -f $Teams[$Counter].Titan1,$Titans[$Teams[$Counter].Titan1].API,$Teams[$Counter].Titan2,$Titans[$Teams[$Counter].Titan2].API,
+        $Teams[$Counter].Titan3,$Titans[$Teams[$Counter].Titan3].API,$Teams[$Counter].Titan4,$Titans[$Teams[$Counter].Titan4].API,$Teams[$Counter].Titan5,$Titans[$Teams[$Counter].Titan5].API) +
+        '},"defenders": [{' +
+        ('"1": {0},"2": {1},"3": {2},"4": {3},"5": {4}' -f  $titans[$Defender.Titan1].api,$titans[$Defender.Titan2].api,$titans[$Defender.Titan3].api,$titans[$Defender.Titan4].api,$titans[$Defender.Titan5].api)+
+        '}],"effects": {"defenders": {},"battleConfig": "titan_clan_pvp"},"reward": [],"seed": 1124393257,"type": "clan_global_pvp_titan","startTime": 1673755322},"noOfBattles": 100,"secretInput": ["auto",0,0,"auto",0,0]}')
+
+        #Win Rate
+        "$($Titans[$Teams[$Counter].Titan1].name)-$($Titans[$Teams[$Counter].Titan2].name)-$($Titans[$Teams[$Counter].Titan3].name)-$($Titans[$Teams[$Counter].Titan4].name)-$($Titans[$Teams[$Counter].Titan5].name) VS " +
+        "$($Titans[$Defender.Titan1].name)-$($Titans[$Defender.Titan2].name)-$($Titans[$Defender.Titan3].name)-$($Titans[$Defender.Titan4].name)-$($Titans[$Defender.Titan5].name): $(($Battle.Content |ConvertFrom-Json).result.successRate)"
+
+        If (($Battle.Content |ConvertFrom-Json).result.wins -eq 100) {
+            $CounterFound = $True
+            $Null = $Results.add([PSCustomObject]@{
+                Attackers   = "$($Titans[$Teams[$Counter].Titan1].name)-$($Titans[$Teams[$Counter].Titan2].name)-$($Titans[$Teams[$Counter].Titan3].name)-$($Titans[$Teams[$Counter].Titan4].name)-$($Titans[$Teams[$Counter].Titan5].name)"
+                Defenders   = "$($Titans[$Defender.Titan1].name)-$($Titans[$Defender.Titan2].name)-$($Titans[$Defender.Titan3].name)-$($Titans[$Defender.Titan4].name)-$($Titans[$Defender.Titan5].name)"
+                Games       = ($Battle.Content |ConvertFrom-Json).result.games
+                Wins        = ($Battle.Content |ConvertFrom-Json).result.wins
+                successRate = ($Battle.Content |ConvertFrom-Json).result.successRate
+            })
+        }
+        $Counter++
+    }
+
+}
